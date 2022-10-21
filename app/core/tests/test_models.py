0bb30e26178f6313ed5124a47bb8e1e31eeaf414
talfour <jak.malinowski94@gmail.com>
2022-10-21 16:44:11 +0100

    Added recipe API and tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Test for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -39,8 +43,19 @@ class ModelTests(TestCase):
 
     def test_create_super_user(self):
         """Test creating a superuser"""
-        user = get_user_model().objects.create_superuser(
-            "test@example.com", "test123"
-            )
+        user = get_user_model().objects.create_superuser("test@example.com", "test123")
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user("test@example.com", "testpass123")
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
